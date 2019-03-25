f8d553476f5e7044bd4f70e6d419e4f133f60b49
FAlkhanaizi <fatema.alkhanaizi@durham.ac.uk>
2019-03-25 15:26:55 +0300

    modified cpu scripts for ncc

--- modnet_cpu.sh
+++ modnet_cpu.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -c 1
-#SBATCH --exclusive
+#SBATCH -c 2
 #SBATCH --mem=8G
 #SBATCH -p "cpu"
 #SBATCH --qos="long-cpu"
-#SBATCH -t 14-0
 
 source /etc/profile
 
--- mono_cpu.sh
+++ mono_cpu.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -N 1
-#SBATCH -c 1
+#SBATCH -c 2
 #SBATCH --mem=8G
 #SBATCH -p "cpu"
 #SBATCH --qos="long-cpu"
-#SBATCH --exclusive
 
 source /etc/profile
 
